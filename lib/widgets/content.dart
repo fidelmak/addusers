f1c558c494f303a6f58c9419df7a6f4cf249f385
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-02-06 23:54:44 +0100

    On branch master

--- lib/widgets/content.dart
+++ lib/widgets/content.dart
@@ -8,6 +8,20 @@ class content extends StatefulWidget {
   State<content> createState() => _contentState();
 }
 
+const _mydummy = [
+  "When you have a dream, you've got to grab it and never let go.",
+  "Nothing is impossible. The word itself says 'I'm possible!'",
+  "There is nothing impossible to they who will try",
+  "The bad news is time flies. The good news is you're the pilot.",
+  "Life has got all those twists and turns. You've got to hold on tight and off you go.",
+  "Be courageous. Challenge orthodoxy. Stand up for what you believe in. When you are in your rocking chair talking to your grandchildren many years from now, be sure you have a good story to tell.",
+  "You make a choice: continue living your life feeling muddled in this abyss of self-misunderstanding, or you find your identity independent of it. You draw your own box."
+];
+
+// define a varaiable with type of int
+int clk = Random().nextInt(6) + 1;
+// using the index of mydummy
+
 class _contentState extends State<content> {
   @override
   Widget build(BuildContext context) {
@@ -15,6 +29,13 @@ class _contentState extends State<content> {
     void set() {
       setState(() {
         click = Random().nextInt(5) + 1;
+      });
+    }
+
+    var firstQuote = _mydummy[clk];
+    void qt() {
+      setState(() {
+        firstQuote = _mydummy[clk];
       });
     }
 
@@ -35,9 +56,9 @@ class _contentState extends State<content> {
                 ),
                 Expanded(
                   child: TextButton(
-                    child: Text('Selectable text'),
+                    child: Text(firstQuote),
                     onPressed: () {
-                      set();
+                      qt();
                     },
                   ),
                 ),
@@ -60,14 +81,24 @@ class _contentState extends State<content> {
                 ),
                 Expanded(
                   child: TextButton(
-                    child: Text('Selectable text'),
+                    child: Text(firstQuote),
                     onPressed: () {
-                      set();
+                      qt();
                     },
                   ),
                 ),
               ]),
         ),
+        SizedBox(height: 30),
+        TextButton(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.blueGrey),
+          ),
+          onPressed: () {
+            qt();
+          },
+          child: Text(firstQuote),
+        )
       ],
     );
   }
